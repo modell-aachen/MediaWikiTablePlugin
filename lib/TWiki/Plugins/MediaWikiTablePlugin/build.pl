adb19612bcadc8cdc049e1eec49386cb7d4f2c9a
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-07 17:39:44 +0000

    Item8039: fixed build script
    git-svn-id: http://svn.foswiki.org/trunk/MediaWikiTablePlugin@1818 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/MediaWikiTablePlugin/build.pl
+++ lib/TWiki/Plugins/MediaWikiTablePlugin/build.pl
@@ -5,10 +5,10 @@ BEGIN {
   }
 }
 
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'MediaWikiTablePlugin' );
+$build = new Foswiki::Contrib::Build( 'MediaWikiTablePlugin' );
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
