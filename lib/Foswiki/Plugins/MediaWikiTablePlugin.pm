16e3f43f9d4e76125f420e2a9b9844561cdc094f
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-12 18:09:13 +0000

    Item8514: using ZonePlugin and shipping minified and compressed css now
    git-svn-id: http://svn.foswiki.org/trunk/MediaWikiTablePlugin@6299 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/MediaWikiTablePlugin.pm
+++ lib/Foswiki/Plugins/MediaWikiTablePlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2006-2009 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2006-2010 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -15,38 +15,29 @@
 package Foswiki::Plugins::MediaWikiTablePlugin;
 
 use strict;
-use vars qw(
-  $VERSION $RELEASE $NO_PREFS_IN_TOPIC $SHORTDESCRIPTION
-  $doneHeader $header
-);
-
-$VERSION = '$Rev$';
-$RELEASE = 'v1.20';
-$NO_PREFS_IN_TOPIC = 1;
-$SHORTDESCRIPTION = 'Format tables the <nop>MediaWiki way';
-
-$header = <<'HERE';
-<link rel="stylesheet" href="%PUBURLPATH%/%SYSTEMWEB%/MediaWikiTablePlugin/style.css" type="text/css" media="all" />
-HERE
+use Foswiki::Func ();
+our $VERSION = '$Rev$';
+our $RELEASE = 'v1.21';
+our $NO_PREFS_IN_TOPIC = 1;
+our $SHORTDESCRIPTION = 'Format tables the <nop>MediaWiki way';
+our $doneInit = 0;
 
 ###############################################################################
 sub initPlugin { 
-  $doneHeader = 0;
+  $doneInit = 0;
   return 1; 
 }
 
 ###############################################################################
 sub commonTagsHandler {
-# text, topic, web
+  # text, topic, web
   handleMWTable($_[2], $_[1], $_[0]) if $_[0] =~ /(^|[\n\r])\s*{\|/;
-
-  return if $doneHeader;
-  $doneHeader = 1 if  $_[0] =~ s/<head>(.*?[\r\n]+)/<head>$1$header/o;
 }
 
 ###############################################################################
 sub handleMWTable {
   require Foswiki::Plugins::MediaWikiTablePlugin::Core;
+  Foswiki::Plugins::MediaWikiTablePlugin::Core::init() unless $doneInit;
   return Foswiki::Plugins::MediaWikiTablePlugin::Core::handleMWTable(@_);
 }
 
--- lib/Foswiki/Plugins/MediaWikiTablePlugin/Core.pm
+++ lib/Foswiki/Plugins/MediaWikiTablePlugin/Core.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2006-2009 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2006-2010 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -15,13 +15,21 @@
 package Foswiki::Plugins::MediaWikiTablePlugin::Core;
 
 use strict;
-
+use Foswiki::Func ();
 use constant DEBUG => 0; # toggle me
 
 ###############################################################################
 sub writeDebug {
   #&Foswiki::Func::writeDebug('- MediaWikiTablePlugin::Core - '.$_[0]) if DEBUG;
   print STDERR '- MediaWikiTablePlugin::Core - '.$_[0]."\n" if DEBUG;
+}
+
+###############################################################################
+sub init {
+  Foswiki::Func::addToZone('head', 'MEDIAWIKITABLEPLUGIN:CSS', <<'HERE');
+<link rel="stylesheet" href="%PUBURLPATH%/%SYSTEMWEB%/MediaWikiTablePlugin/style.css" type="text/css" media="all" />
+HERE
+  }
 }
 
 ###############################################################################
