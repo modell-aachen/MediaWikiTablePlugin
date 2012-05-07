14cf0a4bc9e94440ee167d91e2abc1a9546fda49
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 10:34:12 +0000

    Item11808: reverting perltidy
    git-svn-id: http://svn.foswiki.org/trunk/MediaWikiTablePlugin@14733 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/MediaWikiTablePlugin/Core.pm
+++ lib/Foswiki/Plugins/MediaWikiTablePlugin/Core.pm
@@ -16,175 +16,165 @@ package Foswiki::Plugins::MediaWikiTablePlugin::Core;
 
 use strict;
 use Foswiki::Func ();
-use constant DEBUG => 0;    # toggle me
+use constant DEBUG => 0; # toggle me
 
 ###############################################################################
 sub writeDebug {
-
   #&Foswiki::Func::writeDebug('- MediaWikiTablePlugin::Core - '.$_[0]) if DEBUG;
-    print STDERR '- MediaWikiTablePlugin::Core - ' . $_[0] . "\n" if DEBUG;
+  print STDERR '- MediaWikiTablePlugin::Core - '.$_[0]."\n" if DEBUG;
 }
 
 ###############################################################################
 sub handleMWTable {
 
-    my $i = 0;
-    while ( $_[0] =~
-s/(^|[\n\r])(\s*{\|(?!.*?(^|[\n\r])\s*{\|).*?[\n\r]\s*\|})/$1.&_handleTable($2)/ges
-      )
-    {
-        $i++;
-
-        #writeDebug("### nesting $i");
-    }
+  my $i = 0;
+  while ($_[0] =~ s/(^|[\n\r])(\s*{\|(?!.*?(^|[\n\r])\s*{\|).*?[\n\r]\s*\|})/$1.&_handleTable($2)/ges) {
+    $i++;
+    #writeDebug("### nesting $i");
+  };
 }
 
 ###############################################################################
 sub _handleTable {
-    my $text = shift;
+  my $text = shift;
 
-    #writeDebug("### called _handleTable");
-    return '' unless $text;
+  #writeDebug("### called _handleTable");
+  return '' unless $text;
+  #writeDebug("in:\n'$text'");
 
-    #writeDebug("in:\n'$text'");
+  my @result;
+  my $foundRow = 0;
+  my $foundCell = 0;
+  my $foundHead = 0;
+  my $foundTable = 0;
+  my $foundCaption = 0;
+  my $rowTag = 'oddrow';
 
-    my @result;
-    my $foundRow     = 0;
-    my $foundCell    = 0;
-    my $foundHead    = 0;
-    my $foundTable   = 0;
-    my $foundCaption = 0;
-    my $rowTag       = 'oddrow';
-
-    foreach my $line ( split( /[\n\r]/, $text ) ) {
-
-        # tables
-        if ( $line =~ s/^\s*{\|(.*)$/<table $1>/ ) {    # begin table
-            die "second table found" if $foundTable;
-            unless ( $line =~ s/(class=[\\"'])/$1wikitable / ) {
-                $line =~ s/<table /<table class='wikitable' /;
-            }
-            $foundTable = 1;
-        }
-        if ( $line =~ s/^\s*\|}/<\/td><\/tr>\n<\/table>/ ) {    # end table
-            $foundTable = 0;
-            $foundRow   = 0;
-            $foundCell  = 0;
-            $foundHead  = 0;
-        }
-
-        # cells
-        if ( $line =~ s/^\s*\|([^}\+\-].*)?$/&_handleTableCells($1, 0)/e ) {
-            unless ($foundRow) {
-                $line     = "<tr class=\"$rowTag\">" . $line;
-                $foundRow = 1;
-                $rowTag   = ( $rowTag eq 'oddrow' ) ? 'evenrow' : 'oddrow';
-            }
-            if ($foundCaption) {
-                $line         = "</caption>" . $line;
-                $foundCaption = 0;
-            }
-            if ($foundCell) {
-                $line      = "</td>" . $line;
-                $foundCell = 0;
-            }
-            elsif ($foundHead) {
-                $line      = "</th>" . $line;
-                $foundHead = 0;
-            }
-            $foundCell = 1;
-        }
-
-        # head
-        if ( $line =~ s/^\s*!([^}\+\-].*)$/&_handleTableCells($1, 1)/e ) {
-            unless ($foundRow) {
-                $line     = "<tr>" . $line;
-                $foundRow = 1;
-            }
-            if ($foundCaption) {
-                $line         = "</caption>" . $line;
-                $foundCaption = 0;
-            }
-            $line = "</th>\n$line" if $foundHead;
-            $foundHead = 1;
-        }
-
-        # captions
-        if ( $line =~ s/^\s*\|\+(.*)?$/&_handleTableCells($1, 2)/e ) {
-            if ($foundCell) {
-                $line      = "</td>" . $line;
-                $foundCell = 0;
-            }
-            if ($foundRow) {
-                $line     = "</tr>" . $line;
-                $foundRow = 0;
-            }
-            if ($foundCaption) {
-                $line = "</caption>" . $line;
-            }
-            $foundCaption = 1;
-        }
-
-        # rows
-        if ( $line =~ s/^\s*\|-+(.*)$/<tr $1>/ ) {    # begin row
-            unless ( $line =~ s/(class=[\\"'])/$1$rowTag / ) {
-                $line =~ s/<tr /<tr class=\"$rowTag\" /;
-            }
-            if ($foundCaption) {
-                $line         = "</caption>" . $line;
-                $foundCaption = 0;
-            }
-            $line     = "</tr>\n" . $line if $foundRow;
-            $foundRow = 1;
-            $rowTag   = ( $rowTag eq 'oddrow' ) ? 'evenrow' : 'oddrow';
-            if ($foundCell) {
-                $line      = "</td>" . $line;
-                $foundCell = 0;
-            }
-            elsif ($foundHead) {
-                $line      = "</th>" . $line;
-                $foundHead = 0;
-            }
-        }
-
-        #writeDebug("line=$line");
-        push( @result, $line );
+  foreach my $line (split(/[\n\r]/,$text)) {
+  
+    # tables
+    if ($line =~ s/^\s*{\|(.*)$/<table $1>/) { # begin table
+      die "second table found" if $foundTable;
+      unless ($line =~ s/(class=[\\"'])/$1wikitable /) {
+	$line =~ s/<table /<table class='wikitable' /;
+      }
+      $foundTable = 1;
     }
-    my $result = join( "\n", @result );
+    if ($line =~ s/^\s*\|}/<\/td><\/tr>\n<\/table>/) { # end table
+      $foundTable = 0;
+      $foundRow = 0;
+      $foundCell = 0;
+      $foundHead = 0;
+    }
 
-    #writeDebug("out:\n'$result'");
-    return $result;
+    # cells
+    if ($line =~ s/^\s*\|([^}\+\-].*)?$/&_handleTableCells($1, 0)/e) {
+      unless ($foundRow) {
+	$line = "<tr class=\"$rowTag\">".$line;
+	$foundRow = 1;
+	$rowTag = ($rowTag eq 'oddrow') ? 'evenrow' : 'oddrow'; 
+      }
+      if ($foundCaption) {
+	$line = "</caption>".$line;
+	$foundCaption = 0;
+      }
+      if ($foundCell) {
+	$line = "</td>".$line;
+	$foundCell = 0;
+      } elsif ($foundHead) {
+	$line = "</th>".$line;
+	$foundHead = 0;
+      }
+      $foundCell = 1;
+    }
+
+    # head
+    if ($line =~ s/^\s*!([^}\+\-].*)$/&_handleTableCells($1, 1)/e) {
+      unless ($foundRow) {
+	$line = "<tr>".$line;
+	$foundRow = 1;
+      }
+      if ($foundCaption) {
+	$line = "</caption>".$line;
+	$foundCaption = 0;
+      }
+      $line = "</th>\n$line" if $foundHead;
+      $foundHead = 1;
+    }
+
+    # captions
+    if ($line =~ s/^\s*\|\+(.*)?$/&_handleTableCells($1, 2)/e) {
+      if ($foundCell) {
+	$line = "</td>".$line;
+	$foundCell = 0;
+      }
+      if ($foundRow) {
+	$line = "</tr>".$line;
+	$foundRow = 0;
+      }
+      if ($foundCaption) {
+	$line = "</caption>".$line;
+      }
+      $foundCaption = 1;
+    }
+
+    # rows
+    if ($line =~ s/^\s*\|-+(.*)$/<tr $1>/) { # begin row
+      unless ($line =~ s/(class=[\\"'])/$1$rowTag /) {
+	$line =~ s/<tr /<tr class=\"$rowTag\" /;
+      } 
+      if ($foundCaption) {
+	$line = "</caption>".$line;
+	$foundCaption = 0;
+      }
+      $line = "</tr>\n".$line if $foundRow;
+      $foundRow = 1;
+      $rowTag = ($rowTag eq 'oddrow') ? 'evenrow' : 'oddrow'; 
+      if ($foundCell) {
+	$line = "</td>".$line;
+	$foundCell = 0;
+      } elsif ($foundHead) {
+	$line = "</th>".$line;
+	$foundHead = 0;
+      }
+    }
+
+    #writeDebug("line=$line");
+    push (@result, $line);
+  }
+  my $result = join("\n", @result);
+
+  #writeDebug("out:\n'$result'");
+  return $result;
 }
 
 ##############################################################################
 sub _handleTableCells {
-    my ( $text, $flag ) = @_;
+  my ($text, $flag) = @_;
 
-    #writeDebug("text=$text, flag=$flag");
-    my $cellTag = 'td';
-    $cellTag = 'th'      if $flag == 1;
-    $cellTag = 'caption' if $flag == 2;
-    unless ($text) {
-        return "<$cellTag>";
+  #writeDebug("text=$text, flag=$flag");
+  my $cellTag = 'td';
+  $cellTag = 'th' if $flag == 1;
+  $cellTag = 'caption' if $flag == 2;
+  unless ($text) {
+    return "<$cellTag>";
+  }
+  my @cells;
+  foreach my $cell (split(/!{2}|\|{2}/,$text)) {
+    #writeDebug("cell=$cell");
+    my $params = '';
+    $cell =~ s/<!--/<<nop>!--/go; # take care of html comments
+    if ($cell =~ /^(.*?)(?<!<nop>)[!\|](.*)$/) {
+      $params = ' '.$1;
+      $cell = $2;
     }
-    my @cells;
-    foreach my $cell ( split( /!{2}|\|{2}/, $text ) ) {
+    $cell =~ s/<<nop>!--/<!--/go;
+    push (@cells, "<$cellTag$params>$cell");
+  }
 
-        #writeDebug("cell=$cell");
-        my $params = '';
-        $cell =~ s/<!--/<<nop>!--/go;    # take care of html comments
-        if ( $cell =~ /^(.*?)(?<!<nop>)[!\|](.*)$/ ) {
-            $params = ' ' . $1;
-            $cell   = $2;
-        }
-        $cell =~ s/<<nop>!--/<!--/go;
-        push( @cells, "<$cellTag$params>$cell" );
-    }
-
-    my $result = join( "</$cellTag>\n", @cells );
-
-    #writeDebug("result=$result");
-    return $result;
+  my $result =join("</$cellTag>\n", @cells);
+  #writeDebug("result=$result");
+  return $result;
 }
 
 1;
